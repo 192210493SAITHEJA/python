f4314c54f0ded5468acd16418505c4345770e623
SAI TEJA <128607779+192210493SAITHEJA@users.noreply.github.com>
2024-03-28 10:09:52 +0530

    Add files via upload

---
+++ find odd num within range.py
@@ -0,0 +1,13 @@
+def print_odd_numbers(start, end):
+    # Base case: if the start is greater than end, stop the recursion
+    if start > end:
+        return
+    # Check if the current number is odd
+    if start % 2 != 0:
+        print(start)
+    # Recursively call the function with the next number
+    print_odd_numbers(start + 1, end)
+    
+start = int(input())
+end = int(input())
+print_odd_numbers(start, end)
